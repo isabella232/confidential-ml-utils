eea8e979124c61b223da410a31c13a73b9121d81
Jeff Omhover <jeomhove@microsoft.com>
2020-05-07 22:11:45 -0700

    tests for metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -17,6 +17,7 @@ from unittest.mock import Mock
 
 
 def test_log_metric_public():
+    """ Tests log_metric() for public metric """
     aml_run_wrapper_mock = Mock()
     AmlRunWrapper._instance = aml_run_wrapper_mock  # replacing singleton
 
@@ -34,6 +35,7 @@ def test_log_metric_public():
 
 
 def test_log_metric_private():
+    """ Tests log_metric() for private metric """
     aml_run_wrapper_mock = Mock()
     AmlRunWrapper._instance = aml_run_wrapper_mock  # replacing singleton
 
@@ -50,6 +52,7 @@ def test_log_metric_private():
 
 
 def test_log_row_public():
+    """ Tests log_row() for public metric """
     aml_run_wrapper_mock = Mock()
     AmlRunWrapper._instance = aml_run_wrapper_mock  # replacing singleton
 
@@ -68,6 +71,7 @@ def test_log_row_public():
 
 
 def test_log_row_private():
+    """ Tests log_row() for private metric """
     aml_run_wrapper_mock = Mock()
     AmlRunWrapper._instance = aml_run_wrapper_mock  # replacing singleton
 
@@ -82,3 +86,47 @@ def test_log_row_private():
     )
     aml_run_wrapper_mock.log.assert_not_called()
     aml_run_wrapper_mock.log_row.assert_not_called()
+
+
+def test_log_time_block():
+    """ Tests log_time_block() with statement """
+    aml_run_wrapper_mock = Mock()
+    AmlRunWrapper._instance = aml_run_wrapper_mock  # replacing singleton
+
+    run = AmlRunWrapper()
+    assert isinstance(run, Mock)
+
+    test_key = "my_perf_metric_name"
+
+    with log_time_block(test_key, run=run):
+        print("(((sleeping for 1 second)))")
+        time.sleep(1)
+
+    aml_run_wrapper_mock.log.assert_called_once()
+    aml_run_wrapper_mock.log_row.assert_not_called()
+    metric_args, metric_kwargs = aml_run_wrapper_mock.log.call_args
+    assert metric_args[0] == test_key
+    assert isinstance(metric_args[1], float)
+    assert metric_args[1] >= 0.5
+
+@log_time_function
+def fake_function():
+    """Sleeps for 2 secs"""
+    time.sleep(1)
+
+def test_log_time_function():
+    """ Tests log_time_function decorator """
+    aml_run_wrapper_mock = Mock()
+    AmlRunWrapper._instance = aml_run_wrapper_mock  # replacing singleton
+
+    run = AmlRunWrapper()
+    assert isinstance(run, Mock)
+
+    fake_function()
+
+    aml_run_wrapper_mock.log.assert_called_once()
+    aml_run_wrapper_mock.log_row.assert_not_called()
+    metric_args, metric_kwargs = aml_run_wrapper_mock.log.call_args
+    assert metric_args[0] == "fake_function.time"
+    assert isinstance(metric_args[1], float)
+    assert metric_args[1] >= 0.5
