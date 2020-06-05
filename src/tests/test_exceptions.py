c98a6e69fc3d3ecca77c5fc7300e21555360b7af
Dan Miller <danmill@microsoft.com>
2020-06-05 12:36:16 -0700

    Add three new unit tests

--- src/tests/test_exceptions.py
+++ src/tests/test_exceptions.py
@@ -19,3 +19,44 @@ def test_prefix_stack_trace_preserves_exception_type(message: str, exec_type):
 
     assert message in str(info.value)
     assert SCRUB_MESSAGE in file.getvalue()
+
+
+def test_prefix_stack_trace_respects_disable():
+    file = io.StringIO()
+
+    @prefix_stack_trace(file, disable=True)
+    def function():
+        raise Exception()
+
+    with pytest.raises(Exception):
+        function()
+
+    assert file.getvalue() == ""
+
+
+@pytest.mark.parametrize("prefix", ["foo__"])
+def test_prefix_stack_trace_respects_prefix(prefix):
+    file = io.StringIO()
+
+    @prefix_stack_trace(file, prefix=prefix)
+    def function():
+        raise Exception()
+
+    with pytest.raises(Exception):
+        function()
+
+    assert prefix in file.getvalue()
+
+
+@pytest.mark.parametrize("message", ["foo__"])
+def test_prefix_stack_trace_respects_scrub_message(message):
+    file = io.StringIO()
+
+    @prefix_stack_trace(file, scrub_message=message)
+    def function():
+        raise Exception()
+
+    with pytest.raises(Exception):
+        function()
+
+    assert message in file.getvalue()
